8e0773e7ba8cf52d85166c3e45ca87bc8b63668e
Ballred95 <blake.allred95@icloud.com>
2020-06-24 21:12:34 -0600

    created ridelist component and added required library variables

---
+++ src/components/ridelist.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react' 
+const Themeparks = require("themeparks");
+const DisneyWorldMagicKingdom = new ThemeParks.Parks.DisneylandResortMagicKingdom();
+
+export default class RideList extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+render() {
+        return (
+            <div className = ''>
+                
+            </div>
+        )
+    }
+}
